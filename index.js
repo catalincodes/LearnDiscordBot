ee927d6a4723ace15fd28af0a5217ecbf22f05d5
Catalin Popescu <catalinp86@gmail.com>
2021-01-10 19:03:03 -0500

    * handle multiple mentions

--- index.js
+++ index.js
@@ -41,6 +41,16 @@ client.on('message', message => {
         }
 
         message.channel.send(`Waving at : ${taggedUser.username}`);
+    } else if (message.content.startsWith(`${prefix}avatar`)) {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({format: "png", dynamic: true})}>`);
+        }
+
+        const avatarList = message.mentions.users.map( user => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true})}>`;
+        })
+
+        message.channel.send(avatarList);
     }
 })
 
